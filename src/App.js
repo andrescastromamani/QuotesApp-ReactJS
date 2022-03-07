c169a9616fe62e67e801a28d93b70a1faf3f1e9f
Andrés Castro Mamani <castroandresrm@gmail.com>
2022-03-07 08:57:52 -0400

    List quotes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import { Form } from "./components/Form";
+import { Quote } from "./components/Quote";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
   const saveQuote = (quote) => {
     setQuotes([...quotes, quote]);
   }
+  const deleteQuote = (id) => {
+    setQuotes(quotes.filter(quote => quote.id !== id));
+  }
+  const title = quotes.length > 0 ? "Quotes List" : "Quotes is empty";
   return (
     <div className="container">
       <h1 className="text-center mt-3">Administration Quotes</h1>
       <div className="row mt-5 shadow-lg p-4">
         <div className="col-12 col-md-6 p-5">
+          <h3 className="text-center mb-3">Add Quote</h3>
           <Form saveQuote={saveQuote} />
         </div>
         <div className="col-12 col-md-6 p-5">
-          <p>This is a simple React app.</p>
+          <h3 className="text-center mb-3">{title}</h3>
+          {quotes.map((quote) => (
+            <Quote key={quote.id} quote={quote} deleteQuote={deleteQuote} />
+          ))}
         </div>
       </div>
     </div >
---
+++ src/components/Quote.jsx
@@ -0,0 +1,20 @@
+
+export const Quote = ({ quote, deleteQuote }) => {
+    return (
+        <div className="bg-dark text-white p-3 rounded row mb-2">
+            <div className="col">
+                <p>Name Pet: <span className="text-info">{quote.namePet}</span></p>
+                <p>Name Owner: <span className="text-info">{quote.nameOwner}</span></p>
+                <p>Date: <span className="text-info">{quote.date}</span></p>
+            </div>
+            <div className="col">
+                <p>Hour: <span className="text-info">{quote.hour}</span></p>
+                <p>Symptom: <span className="text-info">{quote.symptom}</span></p>
+            </div>
+            <button
+                className="btn btn-danger"
+                onClick={() => deleteQuote(quote.id)}
+            >Delete</button>
+        </div>
+    )
+}
